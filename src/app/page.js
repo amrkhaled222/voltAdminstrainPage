fe6b316f643bfdf34c4515b81ade68566a5dda45
amrkhaled222 <kamr151515@gmail.com>
2024-05-03 18:01:43 +0300

    solve error

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,6 @@
 import { useRouter } from "next/router";
 import Image from "next/image";
 import Nav from "./_components/Nav";
-import { Router } from "react-router";
 
 export default function Home() {
 	return;
